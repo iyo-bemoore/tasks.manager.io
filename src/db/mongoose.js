b35101528fa25e8791ac0beafa14b0d335a35801
2imad <imadyoussoufi@gmail.com>
2020-03-18 11:18:35 +0100

    setup sendgrid

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 const express = require("express");
 require("dotenv").config();
 
-const mongoClientURI = `${process.env.MONGO_DB_HD}${process.env.MONGO_DB_USR}:${process.env.MONGO_DB_PSWD}${process.env.MONGO_DB_BASE}`;
+const mongoClientURI = `${process.env.MONGO_DB_HD}${process.env.MONGO_DB_USR}:${
+  process.env.MONGO_DB_PSWD
+}${
+  process.env.NODE_ENV === "development"
+    ? process.env.MONGO_DB_BASE
+    : process.env.MONGO_DB_BASE_PRD
+}`;
 mongoose.connect(mongoClientURI, {
   useCreateIndex: true,
   useNewUrlParser: true,
